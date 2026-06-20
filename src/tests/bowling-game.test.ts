f124361e494f5739d3fa213c86c8b68ff62f9ffb
JMendioroz <jmendiorozredin@gmail.com>
2026-06-20 17:55:59 +0200

    refactor calculate total score using frames

--- src/tests/bowling-game.test.ts
+++ src/tests/bowling-game.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, it } from '@jest/globals';
 
+type Frame = {
+  startsAt: number;
+};
+
 class BowlingGame {
   private rolls: number[] = [];
 
@@ -8,7 +12,16 @@ class BowlingGame {
   }
 
   calculateTotalScore() {
-    return this.rolls.reduce((total, roll) => total + roll, 0);
+    return this.frames().reduce((total, frame) => total + this.calculateFrameScore(frame), 0);
+  }
+
+  calculateFrameScore({ startsAt }: Frame) {
+    return (this.rolls.at(startsAt) ?? 0) + (this.rolls.at(startsAt + 1) ?? 0);
+  }
+
+  frames(): Frame[] {
+    const amountOfFrames = 10;
+    return Array.from({ length: amountOfFrames }, (_, i) => ({ startsAt: 2 * i }));
   }
 }
 
@@ -22,6 +35,13 @@ describe('bowling game', () => {
   it('should be able to calculate the total score for game with all ones', () => {
     const game = new BowlingGame();
     rollMany(game, 20, 1);
+    expect(game.calculateTotalScore()).toBe(20);
+  });
+
+  it('should be able to calculate the total score for a game with a spare and an extra non-zero roll', () => {
+    const game = new BowlingGame();
+    rollMany(game, 3, 5);
+    rollMany(game, 17, 0);
     expect(game.calculateTotalScore()).toBe(20);
   });
 });
